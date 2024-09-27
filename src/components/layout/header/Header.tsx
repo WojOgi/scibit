9a6ffb4add0e4ff913828ec38332d2bb56c02da0
Wojciech Ogieglo <w.ogieglo@gmail.com>
2024-09-27 10:40:46 +0300

    added a short website description

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,13 +1,30 @@
 export default function Header() {
   return (
     <>
+      <h1>SciBit - Are Scientists Interested in Bitcoin?</h1>
+      <section>
+        <p>
+          The graphs below are intended to give a rough impression on how
+          popular Bitcoin-related topics are among scientists working at various
+          institutions around the world.
+        </p>
+        <p>
+          The data is based on the number of peer-reviewed articles in the
+          Scopus database that mention the term "Bitcoin" in their title,
+          abstract, or keywords. The numbers of publications are aggregated
+          across all relevant scientific disciplines: Computer Science,
+          Economics, Finance, Business, Management, Mathematics, Decision
+          Sciences, Social Sciences, etc.
+        </p>
+      </section>
       <h2
         style={{
           display: "flex",
           justifyContent: "flex-end",
         }}
       >
-        Number of scientific articles referencing "Bitcoin" (source Scopus)
+        Number of articles referencing "Bitcoin" (right: normalized by a
+        population)
       </h2>
     </>
   );
