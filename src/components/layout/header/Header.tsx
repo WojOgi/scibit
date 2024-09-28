54c9b853fc6e0196e4dac576d9c3c9f4e8a11a7a
Wojciech Ogieglo <w.ogieglo@gmail.com>
2024-09-28 15:41:46 +0300

    fixed minor issues,

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -9,10 +9,21 @@ export default function Header() {
           at various institutions around the world.
         </p>
         <p>
-          The data is based on the number of peer-reviewed articles in the
-          <b>Scopus database</b> that mention the term "Bitcoin" in their title,
-          abstract, or keywords. The numbers of publications are aggregated
-          across all relevant scientific disciplines:{" "}
+          The data is based on the number of peer-reviewed articles in the{" "}
+          <b>
+            <a
+              href={
+                "https://www.scopus.com/results/results.uri?sort=plf-f&src=s&st1=Bitcoin&sid=f81aed54ea56e96dec73bb8ef7124d17&sot=b&sdt=b&sl=22&s=TITLE-ABS-KEY%28bitcoin%29&origin=searchbasic&editSaveSearch=&yearFrom=Before+1960&yearTo=Present&sessionSearchId=f81aed54ea56e96dec73bb8ef7124d17&limit=10"
+              }
+              target={"_blank"}
+            >
+              {" "}
+              Scopus Database (click to check yourself)
+            </a>
+          </b>{" "}
+          that mention the term "Bitcoin" in their title, abstract, or keywords.
+          The numbers of publications are aggregated across all relevant
+          scientific disciplines:{" "}
           <b>
             Computer Science, Economics, Finance, Business, Management,
             Mathematics, Decision Sciences, Social Sciences, and others.
