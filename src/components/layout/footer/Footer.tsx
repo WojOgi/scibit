60771b2a3f3df1cc20c0f934165566e610758317
Wojciech Ogieglo <w.ogieglo@gmail.com>
2024-09-28 10:46:42 +0300

    fixed lighting address issue

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
         <b>Buy me a coffee (lightning)</b>
       </div>
       <img
-        src="../../../styles/lightning_address.png"
+        src="src/styles/lightning_address.png"
         style={{
           width: "9%",
           height: "auto",
